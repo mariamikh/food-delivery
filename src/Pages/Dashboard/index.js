a1fb8d1a741fefd581e671a66cc9ac3d6ddccf1e
Mariam <m.khunjgurua@gmail.com>
2021-11-25 15:06:07 +0400

    change to version 2

---
+++ src/Pages/Dashboard/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useAuthDispatch, logout, useAuthState } from '../../Context';
+import styles from './dashboard.module.css';
+
+function Dashboard(props) {
+  const dispatch = useAuthDispatch();
+  const userDetails = useAuthState();
+
+  const handleLogout = () => {
+    logout(dispatch);
+    props.history.push('/login');
+  };
+  return (
+    <div style={{ padding: 10 }}>
+      <div className={styles.dashboardPage}>
+        <h1>Dashboard</h1>
+        <button className={styles.logoutBtn} onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+      <p>
+        Welcome{' '}
+        {userDetails !== undefined && userDetails.user !== undefined
+          ? userDetails.user.email
+          : ''}
+      </p>
+    </div>
+  );
+}
+
+export default Dashboard;
