4c882e5f9a39ad082a289a0a3e54bb6f885d855b
Mariam <m.khunjgurua@gmail.com>
2022-01-10 17:29:50 +0400

    addionput instead of button for meal quantity

--- src/Pages/Restaurant/Meal/list.js
+++ src/Pages/Restaurant/Meal/list.js
@@ -31,46 +31,21 @@ export default function Meal(props) {
       <div class="col-3">
         <img src={img} class="rounded float-left" />
       </div>
-      <div class="col-6">
+      <div class="col-5">
         <h6>{name}</h6>
         <p>Some description for meal </p>
       </div>
       <h6 class="col-1 align-self-center">{price}$</h6>
-      <div class="col-2 align-self-center">
-        <button
-          type="button"
-          class="btn btn-secondary btn-sm"
-          onClick={() => {
-            changeButton();
-          }}
-        >
-          {btnText}
-        </button>
+      <div class="col-3 align-self-center">
+        <input
+          type="number"
+          class="form-control form-control-sm"
+          id="input1"
+          placeholder="0"
+          min="1"
+          max="99"
+        />
       </div>
     </div>
-
-    // <div className="d-flex flex-column">
-
-    //   {restaurantList.map((r) => (
-    //     <div
-    //       key={r.id}
-    //       className="d-flex flex-nowrap bg-light m-2"
-    //       onClick={() => openDetails(r.id)}
-    //       style={{ cursor: 'pointer' }}
-    //     >
-    //       <div className="p-2">
-    //         <img
-    //           src="https://bit.ly/3xBxOZE"
-    //           className="img-rounded"
-    //           alt="Cinque Terre"
-    //         />
-    //       </div>
-    //       <div className="p-2">
-    //         <h6>{r.name}</h6>
-    //         <p>{r.address} </p>
-    //       </div>
-    //     </div>
-    //   ))}
-    // </div>
   );
 }
