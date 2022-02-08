d6acd73fabfca31b48209abd3ea4e006d9eff693
Mariam <m.khunjgurua@gmail.com>
2022-02-08 17:55:43 +0400

    implement deleteMeal functionality

--- src/Components/Meal/editMeals.js
+++ src/Components/Meal/editMeals.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import EditMeal from './editMealForm';
+import EditMealForm from './editMealForm';
+import MealDataService from '../../services/meal.service';
 
-export default function MealEditable(props) {
+export default function EditMeal(props) {
   // TODO userRole should not be inside meal
   const { id, name, img, price } = props.meal;
-
-  // function changeQuantity(quantity) {
-  //   props.changeTotal(id, price, quantity);
-  // }
+  const { restaurant } = props.restaurant;
+  const [isDeleted, setIsDeleted] = useState(false);
 
   /* 
     TODO: paging
@@ -17,8 +16,23 @@ export default function MealEditable(props) {
     TODO: add real image src 
     */
 
+  function deleteMeal() {
+    MealDataService.delete(id, props.restaurant)
+      .then(() => {
+        setIsDeleted(true);
+      })
+      .catch((error) => {
+        //TODO: handle error
+        console.log(error);
+      });
+  }
+
   return (
-    <div className="row p-2 mb-2 bg-white">
+    <div
+      className={
+        isDeleted ? 'row p-2 mb-2 bg-white d-none' : 'row p-2 mb-2 bg-white'
+      }
+    >
       <div className="col-3">
         <img src={img} alt="" className="rounded float-left" />
       </div>
@@ -28,11 +42,15 @@ export default function MealEditable(props) {
       </div>
       <h6 className="col-2 align-self-center">{price}$</h6>
       <div className="col-1 align-self-center cursor-pointer">
-        <EditMeal restaurant={props.restaurant} mealDetails={props.meal} />
+        <EditMealForm restaurant={props.restaurant} mealDetails={props.meal} />
       </div>
       <div className="col-1 align-self-center ">
         <a href="#" className="pe-auto">
-          <FontAwesomeIcon className="fa-hand-pointer-o" icon={faTrash} />
+          <FontAwesomeIcon
+            className="fa-hand-pointer-o"
+            icon={faTrash}
+            onClick={() => deleteMeal()}
+          />
         </a>
       </div>
     </div>
