7b8af9dcefb869985d8a2cf83b659d33201f329a
Mariam <m.khunjgurua@gmail.com>
2022-02-08 17:40:17 +0400

    bugfix after renames

--- src/Components/Meal/editMeals.js
+++ src/Components/Meal/editMeals.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-// import MyModal from '../../Layout/Modal';
-import EditMeal from './EditMealForm';
+import EditMeal from './editMealForm';
 
 export default function MealEditable(props) {
   // TODO userRole should not be inside meal
